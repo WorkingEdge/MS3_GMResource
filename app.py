bf59571a25d051172fe361ff122afe6680fc2a70
WorkingEdge <jclry76@gmail.com>
2021-04-06 16:11:59 +0000

    Import PyMongo and ObjectId

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
