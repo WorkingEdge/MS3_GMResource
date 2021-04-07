2a9de5a2325b3dedeb428b260cd94aa8e0e34363
WorkingEdge <jclry76@gmail.com>
2021-04-07 12:50:33 +0000

    Prettify only after IDE change to VSCode

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, session, url_for
-from flask_pymongo import PyMongo 
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-
 
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -15,26 +14,12 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_records")
 def get_records():
     records = mongo.db.records.find()
-    return render_template("records.html", records = records)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return render_template("records.html", records=records)
 
 
 if __name__ == "__main__":
