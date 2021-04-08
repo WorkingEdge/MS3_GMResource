d1ad893162648b6313cb7afb588e97c749db20f8
WorkingEdge <jclry76@gmail.com>
2021-04-08 14:00:24 +0000

    Fix Type error, typo

--- app.py
+++ app.py
@@ -49,11 +49,11 @@ def register():
 def login():
     if request.method == "POST":
         user_exists = mongo.db.users.find_one(
-            {"username": request.form.get("username".lower())})
-
+            {"username": request.form.get("username".lower()})
+        
         if user_exists:
             if check_password_hash(
-                user_exists["password"], request.form.get("pasword")):
+                user_exists["password"], request.form.get("password")):
                     session["session_user"] = request.form.get("username").lower()
                     flash("Welcome back{}".format(request.form.get("username")))   
             else:
@@ -61,7 +61,7 @@ def login():
                 return redirect(url_for("login"))
         else:
             flash("The username or password you entered is not correct. Please try again.")
-            return redirect(url_for("login"))
+                return redirect(url_for("login")) 
     return render_template("login.html")
 
 if __name__ == "__main__":
