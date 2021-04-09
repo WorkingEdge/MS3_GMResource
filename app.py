69ba261362db6da3409ef24fd4e74893c8c6c64a
WorkingEdge <jclry76@gmail.com>
2021-04-09 10:21:29 +0000

    Add add_record function to render template

--- app.py
+++ app.py
@@ -91,7 +91,9 @@ def logout():
     session.pop("session_user")
     return redirect (url_for("login"))
 
-
+@app.route("/add_record")
+def add_record():
+    return render_template("add_record.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
